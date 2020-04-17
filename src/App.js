78f9fd3c2e6445276ad132ee7cdc50761c1afc57
Vladimir Nosov <vladimir@what.digital>
2020-04-17 15:26:48 +0700

    updation app with a shop page, collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
     return (
         <div>
-          <HomePage />
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route exact path='/shop' component={ShopPage}/>
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
